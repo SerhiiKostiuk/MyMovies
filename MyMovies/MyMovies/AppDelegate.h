ae4baf054b9e52d95861b562460714b1d3c9b751
Serhii Kostiuk <serhiikostiuk@Serhii-Kostiuk-iMac.local>
2016-12-27 17:01:02 +0200

    add new project

---
+++ MyMovies/MyMovies/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyMovies
+//
+//  Created by Serhii Kostiuk on 27.12.16.
+//  Copyright © 2016 Serhii Kostiuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyMovies/MyMovies/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyMovies
+//
+//  Created by Serhii Kostiuk on 27.12.16.
+//  Copyright © 2016 Serhii Kostiuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
