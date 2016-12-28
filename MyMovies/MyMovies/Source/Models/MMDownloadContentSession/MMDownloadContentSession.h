5525e92e63efacd705dcaa41c07983db2dd61c89
Serhii Kostiuk <serhiikostiuk@Serhii-Kostiuk-iMac.local>
2016-12-28 15:22:02 +0200

    download content session

---
+++ MyMovies/MyMovies/Source/Models/MMDownloadContentSession/MMDownloadContentSession.h
@@ -0,0 +1,13 @@
+//
+//  MMDownloadContentSession.h
+//  MyMovies
+//
+//  Created by Serhii Kostiuk on 28.12.16.
+//  Copyright © 2016 Serhii Kostiuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MMDownloadContentSession : NSObject
+
+@end
---
+++ MyMovies/MyMovies/Source/Models/MMDownloadContentSession/MMDownloadContentSession.m
@@ -0,0 +1,13 @@
+//
+//  MMDownloadContentSession.m
+//  MyMovies
+//
+//  Created by Serhii Kostiuk on 28.12.16.
+//  Copyright © 2016 Serhii Kostiuk. All rights reserved.
+//
+
+#import "MMDownloadContentSession.h"
+
+@implementation MMDownloadContentSession
+
+@end
