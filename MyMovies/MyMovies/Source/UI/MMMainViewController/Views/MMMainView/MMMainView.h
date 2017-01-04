cf42947116f000e5b3ffb0b4d7f18d77fa6623f9
Serhii Kostiuk <serhiikostiuk@Serhii-Kostiuk-iMac.local>
2017-01-04 17:45:14 +0200

    main view

---
+++ MyMovies/MyMovies/Source/UI/MMMainViewController/Views/MMMainView/MMMainView.h
@@ -0,0 +1,14 @@
+//
+//  MMMainView.h
+//  MyMovies
+//
+//  Created by Serhii Kostiuk on 04.01.17.
+//  Copyright © 2017 Serhii Kostiuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMMainView : UIView
+@property (strong, nonatomic) IBOutlet UIView *mainContainerView;
+
+@end
---
+++ MyMovies/MyMovies/Source/UI/MMMainViewController/Views/MMMainView/MMMainView.m
@@ -0,0 +1,21 @@
+//
+//  MMMainView.m
+//  MyMovies
+//
+//  Created by Serhii Kostiuk on 04.01.17.
+//  Copyright © 2017 Serhii Kostiuk. All rights reserved.
+//
+
+#import "MMMainView.h"
+
+@implementation MMMainView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
